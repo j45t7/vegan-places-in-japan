29fe9c7b92f9e4cde24fc7fa6b9552446860fa77
Justyna Marciniak <justy.marciniak@gmail.com>
2022-02-14 08:01:59 +0900

    add icon

--- pages/add-new-place.js
+++ pages/add-new-place.js
@@ -5,7 +5,12 @@ export default function AddNewPlace() {
     <Layout>
       <div className='container mx-auto'>
         <div className='lg:px-20'>
-          <h1 className='text-3xl font-bold p-5 uppercase'>Add New Place</h1>
+          <h1 className='text-3xl font-bold p-5 uppercase'>
+            <span role='img' aria-label='salad' className='pr-2'>
+              🌱
+            </span>
+            Add New Place
+          </h1>
           <p className='px-5 text-gray-500'>
             The submitted location will be added after few days.
           </p>
