6979647293e79b54aa5f83ac1b10d20f786b3978
Justyna Marciniak <justy.marciniak@gmail.com>
2022-02-18 08:40:59 +0900

    Revert "Revert "add icons to the form""
    This reverts commit ad71d2b7b718aa6967b6726fdcc6859d91cfa147.

--- components/FormInput.js
+++ components/FormInput.js
@@ -1,34 +1,20 @@
 import React from 'react'
 
-const FormInput = ({ label, aria, helperText = '', refer, name }) => {
+const FormInput = ({ label, aria, helperText = '', refer, name, icon }) => {
   return (
     <div className='mb-6'>
       <label
         htmlFor='placeName'
         className=' inline-block mb-2 text-green-700 text-lg'
       >
+        <span className='px-2'>{icon}</span>
         {label}
       </label>
       <input
         type='text'
         ref={refer}
         name={name}
-        className='
-          block
-          w-full
-          px-3
-          py-3
-          text-base
-          font-normal
-          text-gray-700
-          bg-white bg-clip-padding
-          border border-solid border-gray-300
-          rounded
-          transition
-          ease-in-out
-          m-0
-          focus:text-gray-700 focus:bg-white focus:border-peach
-          focus:outline-none'
+        className='input'
         aria-describedby={aria}
       />
       <small className='block mt-1 text-sm text-gray-600'>{helperText}</small>
