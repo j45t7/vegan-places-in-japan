beed278d6ce3f077f65c3f0155fa45fe16bc0d5d
Justyna Marciniak <justy.marciniak@gmail.com>
2022-02-18 08:59:40 +0900

    add meta tags

--- pages/add-new-place.js
+++ pages/add-new-place.js
@@ -1,22 +1,41 @@
 import React from 'react'
+import Head from 'next/head'
 import { Form, Layout } from '../components'
 export default function AddNewPlace() {
   return (
-    <Layout>
-      <div className='container mx-auto'>
-        <div className='lg:px-20'>
-          <h1 className='text-3xl font-bold p-5 uppercase'>
-            <span role='img' aria-label='salad' className='pr-2'>
-              🌱
-            </span>
-            Add New Place
-          </h1>
-          <p className='px-5 text-gray-500'>
-            The submitted location will be added after few days.
-          </p>
-          <Form />
+    <div>
+      <Head>
+        <title>Vegan Places in Japan</title>
+        <meta
+          name='description'
+          content='Find the vegan options around Japan!'
+        />
+        <meta property='og:title' content='Vegan Places in Japan' />
+        <meta property='og:description' content='Vegan options around Japan!' />
+        <meta
+          property='og:url'
+          content='https://veganplacesinjapan.vercel.app/add-new-place'
+        />
+        <meta property='og:type' content='website' />
+        <link rel='icon' href='/leaf.svg' />
+      </Head>
+
+      <Layout>
+        <div className='container mx-auto'>
+          <div className='lg:px-20'>
+            <h1 className='text-3xl font-bold p-5 uppercase'>
+              <span role='img' aria-label='salad' className='pr-2'>
+                🌱
+              </span>
+              Add New Place
+            </h1>
+            <p className='px-5 text-gray-500'>
+              The submitted location will be added after few days.
+            </p>
+            <Form />
+          </div>
         </div>
-      </div>
-    </Layout>
+      </Layout>
+    </div>
   )
 }
